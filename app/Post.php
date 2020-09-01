80624e453dc1216016b53ee6423f1d20c6bcbe91
jhonoquendo <32801020+jhonoquendo@users.noreply.github.com>
2020-09-01 16:48:51 -0500

    Configurando Modelo de Post

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    /*protected $hidden = ['title','body'];
+    protected $appends = ['post_name','post_excerpt'];
+
+    public function getPostNameAttribute(){
+        return strtoupper($this->title);
+    }
+
+    public function getPostExcerptAttribute(){
+        return strtoupper(substr($this->title,0,240).'...');
+    }*/
 }
